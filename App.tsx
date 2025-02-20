20cf0e0e563f0fb2df26278d8614b7a6c56f510d
Tran Anh Tuan <tuanta@enterprisenao.com>
2025-02-20 15:48:43 +0700

    bai17: useState

--- App.tsx
+++ App.tsx
@@ -1,94 +1,30 @@
-import { Button, StyleSheet, View } from "react-native";
-// 2.7 import - CONTAINER - dependencies
-import container from "./src/dependencies/dependencies";
-import UserService from "./src/services/UserService";
-import { PostClient } from "./src/networking/Post/PostClient";
-// import { fetchPosts } from "./src/networking/ApiClient";
-// BAI 16 - phần 2 = CALL API = DEPENDENCIES: tối ưu bộ nhớ + dễ maintain
+import { Button, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+
 export default function App() {
-  // 2.8 lấy INSTANCE từ trong CONTAINER
-  const userService = container.get<UserService>("UserService");
-  const postClient = container.get<PostClient>("PostClient");
+  // BÀI 17: USESTATE - của REACT
+  const [count, setCount] = useState<number>(0);
+
   return (
     <View style={styles.container}>
+      <Text>Count: {count}</Text>
+      <View style={{ margin: 10 }}></View>
       <Button
-        title="call api - 1 DEPENDENCIES"
+        title="Tăng 1"
         onPress={() => {
-          // 2.9 sử dụng METHOD trong INSTANCE
-          postClient.fetchPosts();
+          setCount(count + 1);
         }}
       />
       <View style={{ margin: 10 }}></View>
       <Button
-        title="user service hello"
+        title="Giảm 1"
         onPress={() => {
-          // sử dụng METHOD trong INSTANCE
-          userService.hello();
+          setCount(count - 1);
         }}
       />
     </View>
   );
 }
-
-// // BAI 16 - phần 1 = CALL API = INSTANCE: tốn bộ nhớ + khó maintain
-// export default function App() {
-//   // 5. lấy INSTANCE từ trong CONTAINER
-//   const userService = container.get<UserService>("UserService");
-//   return (
-//     <View style={styles.container}>
-//       <Button
-//         title="call api - 2 INSTANCE"
-//         onPress={() => {
-//           // 6. sử dụng METHOD trong INSTANCE
-//           fetchPosts();
-//         }}
-//       />
-//       <View style={{ margin: 10 }}></View>
-//       <Button
-//         title="user service hello"
-//         onPress={() => {
-//           // 6. sử dụng METHOD trong INSTANCE
-//           userService.hello();
-//         }}
-//       />
-//     </View>
-//   );
-// }
-
-// ============= CACH 2 = VIET DEPENDENCIES ENJECTION =============
-
-// export default function App() {
-//   // 5. lấy INSTANCE từ trong CONTAINER
-//   const userService = container.get<UserService>("UserService");
-//   return (
-//     <View style={styles.container}>
-//       <Button
-//         title="service"
-//         onPress={() => {
-//           // 6. sử dụng METHOD trong INSTANCE
-//           userService.hello();
-//         }}
-//       />
-//     </View>
-//   );
-// }
-
-// ============= CACH 1 = VIET từ khóa NEW =============
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Button
-//         title="service"
-//         onPress={() => {
-//           // NHƯỢC ĐIỂM: sử dụng từ khóa "NEW - Ở NHIỀU NƠI"
-//           // bị tràn bộ nhớ + khó quản lý + khó maintain
-//           new UserService().hello();
-//         }}
-//       />
-//     </View>
-//   );
-// }
 
 const styles = StyleSheet.create({
   container: {
