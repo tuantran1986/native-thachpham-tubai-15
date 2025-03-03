2ea9cc0dfc6889822a0ce6509b7cd6e2e9145c95
Tran Anh Tuan <tuanta@enterprisenao.com>
2025-03-03 19:07:10 +0700

    bai 24 - trien khai CUSTOM HOOK

--- AppBai24CustomHook.tsx
+++ AppBai24CustomHook.tsx
@@ -5,32 +5,48 @@ import { PostClient } from "./src/networking/Post/PostClient";
 import { useEffect, useState } from "react";
 import { Post } from "./src/model/Post";
 
-export default function AppBai24CustomHook() {
-  const [posts, setPosts] = useState<Post[]>([]);
-
+// =============== CUSTOM-HOOK ===============
+const useGetPostList = () => {
+  const [data, setData] = useState<Post[]>([]);
   useEffect(() => {
     // 2.8 lấy INSTANCE từ trong CONTAINER
     const postClient = container.get<PostClient>("PostClient");
     postClient.fetchPostsPromise().then((res) => {
-      setPosts(res.data);
+      setData(res.data);
     });
   }, []);
 
-  // ----------- ----------- -----------
+  return [data]; // CYDB: [] = destructoring - STATE (khong can: setState)
+};
+
+export default function AppBai24CustomHook() {
+  // =============== CODE - TRUOC CUSTOM HOOK ===============
+  // const [posts, setPosts] = useState<Post[]>([]);
+  // useEffect(() => {
+  //   // 2.8 lấy INSTANCE từ trong CONTAINER
+  //   const postClient = container.get<PostClient>("PostClient");
+  //   postClient.fetchPostsPromise().then((res) => {
+  //     setPosts(res.data);
+  //   });
+  // }, []);
+
+  // =============== CUSTOM-HOOK ===============
+  const [posts] = useGetPostList(); // STATE: posts - KHÔNG CẦN: setPosts
+
   return (
     <View style={styles.container}>
       <Text>----------- DS POST -----------</Text>
       <View style={{ height: 300, overflow: "scroll" }}>
-        {posts.map((elm, index) => (
-          <Text key={elm.id}>
-            STT: {index}, TITLE: {elm.title}
-          </Text>
-        ))}
+        {posts &&
+          posts.map((elm, index) => (
+            <Text key={elm.id}>
+              STT: {index}, TITLE: {elm.title}
+            </Text>
+          ))}
       </View>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
